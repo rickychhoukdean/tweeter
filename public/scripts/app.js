24245891723c3987cc9226368fc098f16c388c33
Ricky <chhoukdean184@msn.com>
2019-08-15 19:52:09 +0000

    Add comment to JS file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,12 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ //Function to append tweets to the end of the page
 const renderTweets = function(tweets) {
   for (let tweet in tweets) {
     $("#tweets-container").append(createTweetElement(tweets[tweet]));
   }
 };
 
+//Calculates dates by comparing date of tweet to current date
 const dateCalculator = function(tweetTime) {
   let currentDate = new Date();
   let currentTime = currentDate.getTime();
@@ -18,17 +20,17 @@ const dateCalculator = function(tweetTime) {
   let daysElapsed = timeElapsed / (60 * 60 * 24 * 1000);
 
   switch (true) {
-    case daysElapsed >= 1: {
-      return `${Math.round(daysElapsed)} days ago`;
-    }
-    case daysElapsed < 1 && daysElapsed > 1 / 24: {
-      return `${Math.round(daysElapsed * 1000)} hours ago`;
-    }
-    case daysElapsed < 1 && daysElapsed > 1 / 1440: {
-      return `${Math.round(daysElapsed * 1000)} minutes ago`;
-    }
-    default:
-      return "Less than a minute ago";
+  case daysElapsed >= 1: {
+    return `${Math.round(daysElapsed)} days ago`;
+  }
+  case daysElapsed < 1 && daysElapsed > 1 / 24: {
+    return `${Math.round(daysElapsed * 24)} hours ago`;
+  }
+  case daysElapsed < 1 && daysElapsed > 1 / 1440: {
+    return `${Math.round(daysElapsed * 1440)} minutes ago`;
+  }
+  default:
+    return "Less than a minute ago";
   }
 };
 
@@ -40,20 +42,20 @@ const errorMessage = function(messageLength) {
   }
 };
 
+//To escape from HTML injection
 const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
+//Function to use details from user to create a new tweet
 const createTweetElement = function(tweet) {
   let $tweet = `
   <article class="tweet">
     <header>
       <div class="flex">
-        <div class="user"><img src="${tweet.user.avatars}"></i>${
-    tweet.user.name
-  }</div>
+        <div class="user"><span class="smallpic"><img src="${tweet.user.avatars}"></span></i>${tweet.user.name}</div>
        <div class="handle">@${tweet.user.name}</div>
       </div>
       <div class="tweet-body"> ${escape(tweet.content.text)}</div>
@@ -67,16 +69,23 @@ const createTweetElement = function(tweet) {
   return $tweet;
 };
 
+
 $(document).ready(function() {
+
+  //Function to render all the tweets at once
   const loadTweets = function() {
     $.get("/tweets", function(data) {
       renderTweets(data);
     });
   };
 
+
+  //Page initializing by loading all of the tweets in the database and hiding the error message and compose tweet
   loadTweets();
   $(".error-message").slideUp(0);
   $(".new-tweet").slideUp(0);
+  $(".nagivate-up-button").fadeOut(0);
+
 
   $(window).on("click", function() {
     if ($(".error-message").is(":visible")) {
@@ -84,6 +93,7 @@ $(document).ready(function() {
     }
   });
 
+  //Event listener on the tweet button
   $("#tweet-button").on("click", function(event) {
     event.preventDefault();
     const payload = $(this)
@@ -92,9 +102,7 @@ $(document).ready(function() {
 
     let messageLength = payload.length - 5;
     if (messageLength === 0 || messageLength > 140) {
-      $(".error-message")
-        .text(errorMessage(messageLength))
-        .slideDown(0);
+      $(".error-message").text(errorMessage(messageLength)).slideDown(0);
       setTimeout(() => $(".error-message").slideUp("slow"), 5000);
     } else {
       $.post("/tweets", payload, function(data, status) {
@@ -106,17 +114,19 @@ $(document).ready(function() {
     }
   });
 
+  //Navigation button that shows and focuses on the compose tweet textarea
   $(".navbutton").on("click", function() {
     $(".new-tweet")
       .animate(
         {
           height: "toggle"
         },
-        500
+        300
       )
       .focus();
   });
 
+//Navigate up to bring the user to the top of the page
   $(".nagivate-up-button").on("click", function() {
     $("html,body").animate(
       {
@@ -126,13 +136,13 @@ $(document).ready(function() {
     );
   });
 
+  //The button will appear if the user has scrolled 
   $(window).scroll(function() {
     let height = $(window).scrollTop();
-
     if (height > 1) {
-      $(".nagivate-up-button").css("opacity", "100");
+      $(".nagivate-up-button").fadeIn(300);
     } else {
-      $(".nagivate-up-button").css("opacity", "0");
+      $(".nagivate-up-button").fadeOut(300);
     }
   });
 });
